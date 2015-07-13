86d0860d16092ebd0e51d5909ec6be94cb87d511
Carlo Ledesma <cledesma@ewise.com>
2015-07-13 21:10:53 +0800

    filter by tag

--- replicator.py
+++ replicator.py
@@ -4,7 +4,6 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -13,33 +12,43 @@ def hello_world():
 if __name__ == '__main__':
     #app.run()
 
-    print "Hello"
     # Read access keys from environment variables
     aws_access_key_id = os.environ['aws_access_key_id']
     aws_secret_access_key = os.environ['aws_secret_access_key']
     aws_region = os.environ['aws_region']
-
-    key_name = os.environ['key_name']
-    instance_type = os.environ['instance_type']
-    security_group_ids = os.environ['security_group_ids']
+    aws_instance_name_tag = os.environ['aws_instance_name_tag']
 
     # Get EC2 Connection
     conn = boto.ec2.connect_to_region(aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-    print conn
     # Get reference to master machine
-    instances = conn.get_all_instances()
-    print instances
+    instances = conn.get_only_instances()
+    security_group_ids = []
+    for instance in instances:
+        if (instance.tags['Name'] == aws_instance_name_tag):
+            instance_image_id = instance.image_id
+            instance_type = instance.instance_type
+            instance_key_name = instance.key_name
+            instance_groups = instance.groups
+            for group in instance_groups:
+                security_group_ids.append(group.id)
+            instance_placement = instance.placement 
+
+    print instance_image_id
+    print instance_type
+    print instance_key_name
+    print security_group_ids
+    print instance_placement
     # Get security group of master machine
     # Get keypair of master machine
     # Get vpc of master machine
     # Create AMI name
     # Create an AMI from master machine
     # Create an instance from AMI
-    image_id=image_id
-    instance_type=instance_type
-    key_name=key_name
-    security_group_ids=security_group_ids
-    placement=aws_region
-    monitoring_enabled=true
+    #image_id=image_id
+    #instance_type=instance_type
+    #key_name=key_name
+    #security_group_ids=security_group_ids
+    #placement=aws_region
+    #monitoring_enabled=true
     # Get public IP of new AMI
     # Deregister AMI
